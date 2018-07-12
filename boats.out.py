e07ea127cb9b42c82422ae45604e1b989308a714
Billy O'Mahony <billy.omahony@gmail.com>
2018-07-12 17:07:59 +0100

    sailaway: Adding bunch of files. Some temp

---
+++ boats.out.py
@@ -0,0 +1,11 @@
+[
+Boat ("dc", dc, "Billy O", "dc", 1020), 
+Boat ("dc", dc, "Brian P", "dc", 1024), 
+Boat ("dc", dc, "Des Mc", "dc", 1009), 
+Boat ("dc", dc, "Hugh W", "dc", 999), 
+Boat ("dc", dc, "Jim O", "dc", 988), 
+Boat ("dc", dc, "Margaret H", "dc", 972), 
+Boat ("dc", dc, "Mike H", "dc", 1040), 
+Boat ("dc", dc, "Niamh E", "dc", 1080), 
+Boat ("dc", dc, "Sarah B", "dc", 1020), 
+Boat ("dc", dc, "Tom Mc", "dc", 1000)]
